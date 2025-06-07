30ee50993b51e60fb26fe73b038dea89180a4dbd
Ryaaad <devil.fine116@gmail.com>
2025-06-07 14:34:32 +0100

    feat(routes setup)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import RootLayout from "./components/Rootlayout.tsx";
+import AnalyzeDDi from "./pages/analyseDDI.tsx";
+import DatasetsPage from "./pages/datasets.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+const routes = { extract: "/", datasets: "/datasets" };
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<RootLayout />}>
+            <Route path={routes.extract} element={<AnalyzeDDi />} />
+            <Route path={routes.datasets} element={<DatasetsPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  </StrictMode>
+);
